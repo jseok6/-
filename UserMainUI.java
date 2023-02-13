251d2a335fc348ee2844a44315a99ba69c96f906
jseok6 <jsoekdf@gmail.com>
2023-02-13 10:23:09 +0900

    첫번째작업

---
+++ UserMainUI.java
@@ -0,0 +1,23 @@
+package study;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserMainUI {
+	
+	private JLabel RemainTime;
+	private JLabel notification;
+	
+	
+	public UserMainUI() {
+		
+	}
+
+	public static void main(String[] args) {
+		new UserMainUI();
+
+	}
+
+}
